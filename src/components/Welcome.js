40970704e5a2abfb7aa9d57ad82642b6aec166b7
Shyam Gupta <shyamsundar.g@kiprosh.com>
2021-06-30 00:35:43 +0530

    basic styling

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,9 @@
 function Welcome(data) {
   return (
-    <>
+    <section style={{ padding: "7rem 1rem" }}>
       <h1>{`Hello Shyam`}</h1>
       <h2>{`Current time is ${data?.time}`}</h2>
-    </>
+    </section>
   );
 }
 
